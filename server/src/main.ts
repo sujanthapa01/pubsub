cfdecce6b71848833180360b8112bac3f2fc822d
SujanThapa <158448355+sujanthapa01@users.noreply.github.com>
2026-06-25 11:40:54 +0530

    updated main server file with cors and cookieParser middleware

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import cookieParser from 'cookie-parser'
+
 
 async function bootstrap() {
+
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: "http://localhost:3001",
+    credentials: true,
+  });
+ 
+ 
+  app.use(cookieParser())
+ 
   await app.listen(process.env.PORT ?? 3000);
+
 }
 bootstrap();
