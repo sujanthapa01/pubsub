7d6f01f82f872bda88f079883a110f9afa72806d
SujanThapa <158448355+sujanthapa01@users.noreply.github.com>
2026-08-08 00:08:50 +0530

    updated: added quote-repository and service

--- server/src/quote/quote.module.ts
+++ server/src/quote/quote.module.ts
@@ -3,10 +3,10 @@ import { QuoteController } from './quote.controller';
 import { QuoteService } from './quote.service';
 import { ScheduleModule } from '@nestjs/schedule';
 import {RedisService} from "../redis/redis.service"
-
+import {WriteQuoteService} from "./services/write-quote.service"
 @Module({
   imports : [ScheduleModule.forRoot()],
   controllers: [QuoteController],
-  providers: [QuoteService,RedisService]
+  providers: [QuoteService,RedisService,WriteQuoteService]
 })
 export class QuoteModule {}
