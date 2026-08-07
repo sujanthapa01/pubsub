be6164e26045efb087351be5f421b9942787eecf
SujanThapa <158448355+sujanthapa01@users.noreply.github.com>
2026-08-08 00:06:42 +0530

    added quote repository for better structure

---
+++ server/src/quote/repository/quote.repository.ts
@@ -0,0 +1,12 @@
+import {Injectable} from "@nestjs/common"
+import {DatabaseService} from "../../database/database.service"
+
+@Injectable()
+export class QuoteRepository {
+    constructor(private readonly db: DatabaseService){}
+
+
+    fid
+
+
+}
