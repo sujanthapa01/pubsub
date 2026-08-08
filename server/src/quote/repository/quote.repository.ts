2084573d722683dc560ac07075921b7a5dbd8e67
SujanThapa <158448355+sujanthapa01@users.noreply.github.com>
2026-08-08 11:52:51 +0530

    refactor: implement IQuoteRepository interface in QuoteRepository

--- server/src/quote/repository/quote.repository.ts
+++ server/src/quote/repository/quote.repository.ts
@@ -1,12 +1,23 @@
-import {Injectable} from "@nestjs/common"
-import {DatabaseService} from "../../database/database.service"
+import { Injectable, NotFoundException } from "@nestjs/common"
+import { DatabaseService } from "../../database/database.service"
+import { CreateQuoteData, IQuoteRepository } from "../repository/types/quote-repository.types"
+import { Quote } from "generated/prisma/browser"
 
 @Injectable()
-export class QuoteRepository {
-    constructor(private readonly db: DatabaseService){}
+export class QuoteRepository implements IQuoteRepository {
+    constructor(private readonly db: DatabaseService) { }
+
+    async create(data: CreateQuoteData): Promise<Quote> {
+        return this.db.quote.create({ data })
+    }
 
 
-    fid
+    async findById(id: string): Promise<Quote> {
+        const quote = await this.db.quote.findUnique({ where: { id } })
 
+        if (!quote) { throw new NotFoundException(`Quote id:${id} not found in database`) }
+
+        return quote
+    }
 
 }
